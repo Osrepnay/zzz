316ba83fd4aaec0a66454fa01af1f8a292240fe6
Llerraf Gnem <lleerraaff@gmail.com>
2026-06-23 22:09:08 -0500

    the tests were bad, they pass now

--- test/max-bytes.sh
+++ test/max-bytes.sh
@@ -3,13 +3,13 @@
 
 . test/setup.sh
 
-max_chars=100
+max_chars=1000
 
 # @ should never show up in metadata
 signal_over="@over"
 yes $signal_over | head -c $((max_chars + 1)) | wl-copy
 sleep 0.1
-if ! build/zzzclip list | grep -Fxq $signal_over; then
+if build/zzzclip list | grep -Fq $signal_over; then
     echo 'fail: text over max-item-bytes was stored'
     exit 1
 fi
@@ -18,7 +18,7 @@ fi
 signal_under="@under"
 yes $signal_under | head -c $max_chars | wl-copy
 sleep 0.1
-if ! build/zzzclip list | grep -Fxq $signal_under; then
+if ! build/zzzclip list | grep -Fq $signal_under; then
     echo 'fail: text at max-item-bytes was not stored'
     exit 1
 fi
